b5b627b09da17f8b90b8630e328d62dfc2264adf
Iftekher Sunny <iftekhersunny@hotmail.com>
2018-01-29 10:21:33 +0600

    Refactoring

--- image.js
+++ image.js
@@ -1,1 +1,59 @@
-!function(t){var o=this,n=o.Assets||{name:"assets-helper",version:"0.0.0"};return n.image=function(t){var n=o.location.hostname,e=80==o.location.port?"":":"+o.location.port,r=o.location.protocol,s=["http","https","//"];for(key in s)if(t.indexOf(s[key])!=-1)return t;return r+"//"+n+e+"/"+t},o.Assets=n}();
+// ----------------------------------------------------------------------------
+// Assets Helper, a Javascript Assets Helper library
+// Licensed under the MIT license.
+// ----------------------------------------------------------------------------
+// Copyright (C) Iftekher Sunny < iftekhersunny@hotmail.com >
+// ----------------------------------------------------------------------------
+(function (_) {
+
+
+    /////////////////////////////////////////////////////////
+    //
+    // instance of global object
+    //
+    /////////////////////////////////////////////////////////
+    var root = this;
+
+
+    
+    /////////////////////////////////////////////////////////
+    //
+    // instance of assets object
+    //
+    /////////////////////////////////////////////////////////
+    var Assets = root.Assets || {
+        name: "assets-helper",
+        version: "0.0.0"
+    };
+
+
+
+    /////////////////////////////////////////////////////////
+    //
+    // getting image url
+    //
+    /////////////////////////////////////////////////////////
+    Assets.image = function(path) {
+        var hostname = root.location.hostname,
+            port = root.location.port == 80 ? "" : ":" + root.location.port,
+            protocol = root.location.protocol,
+            protocols = ['http', 'https', '//'];
+
+        
+        for(key in protocols) {
+            if (path.indexOf(protocols[key]) != -1) return path;
+        }
+
+       return protocol + "//" + hostname + port + "/" + path;   
+    }
+
+
+
+    /////////////////////////////////////////////////////////
+    //
+    // assigning Assets object to the Global object
+    //
+    /////////////////////////////////////////////////////////
+    return root.Assets = Assets;
+
+}())
